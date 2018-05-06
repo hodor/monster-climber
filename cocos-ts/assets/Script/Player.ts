a9564b9169d9084e6fa7838a4b7e35e64999f5c2
Rogerio Gasi <rgasi@gazeus.com>
2018-05-06 19:01:23 -0300

    Changing player to add dragon bones animation

--- cocos-ts/assets/Script/Player.ts
+++ cocos-ts/assets/Script/Player.ts
@@ -10,7 +10,7 @@ import ScoreSmall from "./ui/ScoreSmall";
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Player extends cc.Component {
@@ -21,36 +21,36 @@ export default class Player extends cc.Component {
     @property
     targetMaxDist = 0.7;
     @property(cc.Prefab)
-    scoreUI:cc.Prefab = null;
+    scoreUI: cc.Prefab = null;
     @property
     maxDistForPerfect = 10;
     @property(cc.AudioClip)
-    PressAndHoldSound:cc.AudioClip = null;
+    PressAndHoldSound: cc.AudioClip = null;
     @property
-    loopSoundStart:number = 2.73;
+    loopSoundStart: number = 2.73;
     @property
-    loopSoundEnd:number = 3.759;
+    loopSoundEnd: number = 3.759;
     @property(cc.AudioClip)
-    NoScoreLanding:cc.AudioClip = null;
+    NoScoreLanding: cc.AudioClip = null;
     @property(cc.AudioClip)
-    Landing:cc.AudioClip = null;
+    Landing: cc.AudioClip = null;
     @property(cc.AudioClip)
-    PerfectLanding:cc.AudioClip = null;
+    PerfectLanding: cc.AudioClip = null;
     @property(cc.AudioClip)
-    Fail:cc.AudioClip = null;
+    Fail: cc.AudioClip = null;
     // onLoad () {}
     jumpDuration = 0.4;
     jumpMaxPower = 10;
 
     squashDuration = 0.1;
-    
+
     //Make a state machine if this gets too complicated
     isSquashing = false;
 
     maxSquash = 0.2;
     squashAnimation: cc.ActionInterval = null;
 
-    curSquash:number = 1;
+    curSquash: number = 1;
 
     public static initialPos: cc.Vec2 = null;
     initialScale: number = null;
@@ -58,25 +58,33 @@ export default class Player extends cc.Component {
     jitterOffset: number = 100;
     lastDistanceJumped: number = 0;
 
-    private audioSource:cc.AudioSource = null;
+    private armatureDisplay: dragonBones.ArmatureDisplay;
+    private armature: dragonBones.Armature;
 
-    start () {
+    private audioSource: cc.AudioSource = null;
+
+    start() {
         Player.initialPos = this.node.getPosition();
+        this.armatureDisplay = this.node.getComponent(dragonBones.ArmatureDisplay);
+        this.armature = this.armatureDisplay.armature();
+        this.armatureDisplay.addEventListener(dragonBones.EventObject.COMPLETE, this.dragonBonesComplete, this);
+
+        cc.log('armature: %o', this.armature);
         this.initialScale = this.node.getScale();
         this.jumpMaxPower = cc.winSize.height;
         this.audioSource = this.node.getComponent(cc.AudioSource);
     }
 
-    update (dt) {
-        if(this.isSquashing){
-            if(this.node.scaleY <= this.maxSquash) {
+    update(dt) {
+        if (this.isSquashing) {
+            if (this.node.scaleY <= this.maxSquash) {
                 this.curSquash = this.maxSquash;
                 //make it jitter
                 var newX = Player.initialPos.x + Math.sin(Date.now()) * dt * this.jitterOffset;
                 this.node.setPositionX(newX);
             }
-            if(this.audioSource.isPlaying) {
-                if(this.audioSource.getCurrentTime() >= this.loopSoundEnd)
+            if (this.audioSource.isPlaying) {
+                if (this.audioSource.getCurrentTime() >= this.loopSoundEnd)
                     this.audioSource.setCurrentTime(this.loopSoundStart);
             }
         }
@@ -87,37 +95,53 @@ export default class Player extends cc.Component {
         this.isSquashing = false;
         this.node.stopAllActions();
         this.lastDistanceJumped = this.jumpMaxPower * power;
-        var jumpUp = cc.moveBy(0.5, cc.p(0,this.lastDistanceJumped)).easing(cc.easeCubicActionOut());
-        var stretch = cc.scaleTo(this.squashDuration, this.initialScale, this.initialScale + .2);
+        var jumpUp = cc.moveBy(0.5, cc.p(0, this.lastDistanceJumped)).easing(cc.easeCubicActionOut());
+        /*
+        var stretch = cc.scaleTo(this.squashDuration, this.initialScale, this.initialScale + .1);
         var scaleBack = cc.scaleTo(this.squashDuration, this.initialScale, this.initialScale);      
-        this.node.runAction(cc.sequence(stretch, jumpUp, scaleBack, cc.callFunc(callback, callbackTarget)));
+        this.node.runAction(cc.sequence(stretch, jumpUp, scaleBack, cc.callFunc(this.jumpOver, this), cc.callFunc(callback, callbackTarget)));
+        */
+        this.node.runAction(cc.sequence(jumpUp, cc.callFunc(this.jumpOver, this), cc.callFunc(callback, callbackTarget)));
+        this.armatureDisplay.playAnimation('release', -1);
+    }
+
+    jumpOver() {
+        this.armatureDisplay.playAnimation('idle', -1);
     }
 
     startSquash(maxHoldTime) {
         this.curSquash = 1;
         this.isSquashing = true;
-        this.squashAnimation = cc.scaleTo(maxHoldTime/1000, 1, this.maxSquash);
-        this.node.runAction(this.squashAnimation);
+
+        //this.squashAnimation = cc.scaleTo(maxHoldTime / 1000, 1, this.maxSquash);
+        //this.node.runAction(this.squashAnimation);
+      
+        this.armatureDisplay.playAnimation('hold', 1);
         this.playPressAndHold();
+    }
+
+    dragonBonesComplete() {
+        if (this.armatureDisplay.animationName === 'hold')
+            this.armatureDisplay.playAnimation('pressing', -1);
     }
 
     getDistance() {
         return this.lastDistanceJumped;
     }
 
-    moveDownBy(amount, time, callback, target){
+    moveDownBy(amount, time, callback, target) {
         this.node.stopAllActions();
         var movement = cc.moveBy(time, 0, amount);
         this.node.runAction(cc.sequence(movement, cc.callFunc(callback, target)));
     }
 
-    safePoint:cc.Vec2 = null;
+    safePoint: cc.Vec2 = null;
 
     // Get the next target point
     getNextTarget() {
         var targetDist = (this.targetMinDist + (Math.random() * (this.targetMaxDist - this.targetMinDist))) * this.jumpMaxPower;
         var targetYPos = Player.initialPos.y + targetDist;
-        this.safePoint = cc.v2(0,targetYPos);
+        this.safePoint = cc.v2(0, targetYPos);
         return this.safePoint;
     }
 
@@ -125,8 +149,8 @@ export default class Player extends cc.Component {
         return cc.v2(0, Player.initialPos.y + this.getDistance());
     }
 
-    getMaxLandPoint(){
-        return cc.v2(0, Player.initialPos.y + (this.targetMaxDist*this.jumpMaxPower));
+    getMaxLandPoint() {
+        return cc.v2(0, Player.initialPos.y + (this.targetMaxDist * this.jumpMaxPower));
     }
 
     scoreMultiplier: number = 1;
@@ -134,12 +158,12 @@ export default class Player extends cc.Component {
         var dist = (Math.abs(this.safePoint.y - this.getLandedPoint().y));
         var score = 1;
         // Check if we are indeed in the green area
-        if(dist > totalSafeHeight/2){
+        if (dist > totalSafeHeight / 2) {
             score = 0;
             this.playNoScoreLanding();
             this.scoreMultiplier = 1;
         } else {
-            if(dist <= this.maxDistForPerfect){
+            if (dist <= this.maxDistForPerfect) {
                 this.playPerfectLanding();
                 score = 2 * this.scoreMultiplier;
                 this.scoreMultiplier++;
@@ -155,41 +179,41 @@ export default class Player extends cc.Component {
         return score;
     }
 
-    resetScore(){
+    resetScore() {
         this.scoreMultiplier = 1;
     }
 
-    playPressAndHold(){
+    playPressAndHold() {
         this.stopAllSounds();
         this.audioSource.clip = this.PressAndHoldSound;
         this.audioSource.play();
     }
 
-    playLanding(){
+    playLanding() {
         this.stopAllSounds();
         this.audioSource.clip = this.Landing;
         this.audioSource.play();
     }
 
-    playPerfectLanding(){
+    playPerfectLanding() {
         this.stopAllSounds();
         this.audioSource.clip = this.PerfectLanding;
         this.audioSource.play();
     }
 
-    playNoScoreLanding(){
+    playNoScoreLanding() {
         this.stopAllSounds();
         this.audioSource.clip = this.NoScoreLanding;
         this.audioSource.play();
     }
 
-    playDeath(){
+    playDeath() {
         this.stopAllSounds();
         this.audioSource.clip = this.Fail;
         this.audioSource.play();
     }
 
-    stopAllSounds(){
+    stopAllSounds() {
         this.audioSource.stop();
     }
 
